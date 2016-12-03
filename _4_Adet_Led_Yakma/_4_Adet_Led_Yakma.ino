43d427fa63fa5bdd745790c07aded9e6b477bf6e
Faruk ÜNAL <unalfaruk@outlook.com>
2016-12-03 22:25:26 +0300

    LED HIGH hatası düzeltildi.

--- _4_Adet_Led_Yakma/_4_Adet_Led_Yakma.ino
+++ _4_Adet_Led_Yakma/_4_Adet_Led_Yakma.ino
@@ -7,18 +7,18 @@ void setup() {
 }
 
 void loop() {
-  //LED leri yak.
+  //LED leri yak.  
   digitalWrite(13, HIGH); 
   digitalWrite(12, HIGH);  
   digitalWrite(11, HIGH);  
-  digitalWrite(10, HIGH);
+  digitalWrite(10, HIGH); 
   //Bekleme süresini ayarla
   delay(2000);
   //LED leri söndür.                  
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(10, HIGH);
+  digitalWrite(13, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
   //Bekleme süresini ayarla. 
   delay(2000);                      
 }
