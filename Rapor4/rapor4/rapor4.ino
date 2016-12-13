3939eccbf461f133262432175e34784b2a10dccd
Faruk ÜNAL <unalfaruk@outlook.com>
2016-12-13 21:37:54 +0300

    Sicaklik alindi ve ekrana yazdirildi.

--- Rapor4/rapor4/rapor4.ino
+++ Rapor4/rapor4/rapor4.ino
@@ -43,19 +43,30 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+int GelenDeger;
+int GelenDegerGerilim;
+int Sicaklik;
+
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.print("Hoş geldiniz.");
+  //2 saniye bekletelim, hoş geldiniz yazısından sonra
+  delay(2000);
+  lcd.print("Sıcaklık:");
 }
 
 void loop() {
-  // Turn off the display:
-  lcd.noDisplay();
-  delay(500);
-  // Turn on the display:
-  lcd.display();
-  delay(500);
+  GelenDeger = analogRead(A0);               //A0 pininden gelen değeri alalım
+  GelenDegerGerilim=(GelenDeger/1023)*5000; //0-1023 arası değer gelecektir. 0 = 0V / 1023=5V temsil eder.
+                                            // bu değeri mV cinsinden alıyoruz.
+  Sicaklik=GelenDegerGerilim/10,0;          //Sensör 1 C için 10mV değeri verdiği için, gerilimi 10 a bölerek
+                                              //sıcaklık değerini hesaplıyoruz 
+  lcd.setCursor(0,1); //0 sütun, 1 satır numarası. (saymaya 0 dan başladığımız için aslında 2.satırı temsil eder.)
+  lcd.print(Sicaklik);
+  lcd.print(" Derece");
+  delay(1000);
+                                  
 }
 
